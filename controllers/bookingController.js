494e1d56ebece40a7fe9dbeca5c42dbddb82bff0
jbhavyapriya <93140450+jbhavyapriya@users.noreply.github.com>
2024-07-14 10:18:10 +0530

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -8,17 +8,10 @@ import * as factory from "./handlerFactory.js";
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const getCheckoutSession = catchAsync(async (req, res, next) => {
-  // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
-  // console.log(tour);
-
-  // 2) Create checkout session
+ 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-    //   req.params.tourId
-    // }&user=${req.user.id}&price=${tour.price}`,
 
     success_url: `${req.protocol}://${req.get("host")}/my-tours?alert=booking`,
 
@@ -40,7 +33,6 @@ export const getCheckoutSession = catchAsync(async (req, res, next) => {
     ]
   });
 
-  // 3) Create session as response
   res.status(200).json({
     status: "success",
     session
